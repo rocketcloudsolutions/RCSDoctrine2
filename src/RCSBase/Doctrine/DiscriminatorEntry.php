f809fdabd130b02224b5fe6b4b52980491269668
Dustin Moorman <dustin.moorman@gmail.com>
2014-06-04 21:00:33 -0500

    adding RCSBase\Doctrine\DiscriminatorEntry

---
+++ src/RCSBase/Doctrine/DiscriminatorEntry.php
@@ -0,0 +1,17 @@
+<?php
+namespace RCSBase\Doctrine;
+
+class DiscriminatorEntry 
+{  
+    private $value;
+
+    public function __construct( array $data ) 
+    {  
+        $this->value = $data['value'];  
+    }  
+
+    public function getValue() 
+    {  
+        return $this->value;  
+    }  
+}  
